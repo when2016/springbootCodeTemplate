5c7fdbdaf29847b42a3d77576a9ed8bfde9ad076
Wanghongen <wanghongen@localhost.localdomain>
2018-05-09 22:05:43 +0800

    format

--- src/main/java/cn/xiaowenjie/common/aop/ControllerAOP.java
+++ src/main/java/cn/xiaowenjie/common/aop/ControllerAOP.java
@@ -14,51 +14,53 @@ import cn.xiaowenjie.common.exceptions.UnloginException;
 
 /**
  * 处理和包装异常
- * 
+ *
  * @author 肖文杰
  */
 @Aspect
 @Component
 public class ControllerAOP {
-	private static final Logger logger = LoggerFactory.getLogger(ControllerAOP.class);
 
-	@Pointcut("execution(public cn.xiaowenjie.common.beans.ResultBean *(..))")
-    public void controllerMethod(){}
-	
-	@Around("controllerMethod()")
-	public Object handlerControllerMethod(ProceedingJoinPoint pjp) {
-		long startTime = System.currentTimeMillis();
+  private static final Logger logger = LoggerFactory.getLogger(ControllerAOP.class);
 
-		ResultBean<?> result;
+  @Pointcut("execution(public cn.xiaowenjie.common.beans.ResultBean *(..))")
+  public void controllerMethod() {
+  }
 
-		try {
-			result = (ResultBean<?>) pjp.proceed();
-			logger.info(pjp.getSignature() + "use time:" + (System.currentTimeMillis() - startTime));
-		} catch (Throwable e) {
-			result = handlerException(pjp, e);
-		}
+  @Around("controllerMethod()")
+  public Object handlerControllerMethod(ProceedingJoinPoint pjp) {
+    long startTime = System.currentTimeMillis();
 
-		return result;
-	}
+    ResultBean<?> result;
 
-	private ResultBean<?> handlerException(ProceedingJoinPoint pjp, Throwable e) {
-		ResultBean<?> result = new ResultBean();
+    try {
+      result = (ResultBean<?>) pjp.proceed();
+      logger.info(pjp.getSignature() + "use time:" + (System.currentTimeMillis() - startTime));
+    } catch (Throwable e) {
+      result = handlerException(pjp, e);
+    }
 
-		// 已知异常
-		if (e instanceof CheckException) {
-			result.setMsg(e.getLocalizedMessage());
-			result.setCode(ResultBean.FAIL);
-		} else if (e instanceof UnloginException) {
-			result.setMsg("Unlogin");
-			result.setCode(ResultBean.NO_LOGIN);
-		} else {
-			logger.error(pjp.getSignature() + " error ", e);
+    return result;
+  }
 
-			//TODO 未知的异常，应该格外注意，可以发送邮件通知等
-			result.setMsg(e.toString());
-			result.setCode(ResultBean.FAIL);
-		}
+  private ResultBean<?> handlerException(ProceedingJoinPoint pjp, Throwable e) {
+    ResultBean<?> result = new ResultBean();
 
-		return result;
-	}
+    // 已知异常
+    if (e instanceof CheckException) {
+      result.setMsg(e.getLocalizedMessage());
+      result.setCode(ResultBean.FAIL);
+    } else if (e instanceof UnloginException) {
+      result.setMsg("Unlogin");
+      result.setCode(ResultBean.NO_LOGIN);
+    } else {
+      logger.error(pjp.getSignature() + " error ", e);
+
+      //TODO 未知的异常，应该格外注意，可以发送邮件通知等
+      result.setMsg(e.toString());
+      result.setCode(ResultBean.FAIL);
+    }
+
+    return result;
+  }
 }
